a1890d026fac9bdf3fbde68efd0712ce71297db7
Minojini <minojini05@gmail.com>
2025-02-16 13:22:09 +0530

    finall push

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Spotifyclone/"
+  base: "/Spotifyclone/",
+  server: {
+    host: "0.0.0.0",  // Ensure it listens on all network interfaces
+    port: process.env.PORT || 3000  // Use Render's assigned port or fallback to 3000
+  }
 })
